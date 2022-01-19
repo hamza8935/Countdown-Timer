3a2c144edf7a0a4b916aa12f10b23522bc0dd84a
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-01-19 19:36:52 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@
 let min = 1 ;
 let sec = 1 ;
 let msec = 1 ;
-
+let interval ;
 
 let min1 = document.getElementById('min') ;
 let sec1 = document.getElementById('sec') ;
@@ -13,46 +13,52 @@ let btn3 = document.getElementById('btn3') ;
 
 
 btn1.addEventListener('click',function () {
+interval = setInterval(() => {
     
+  
 btn1.disabled = true ;
 
-function timer( ) {
+//function timer( ) {
     msec1.innerText = msec++ ;
     if (msec > 100) {
     
         sec1.innerText = sec++ ;
         msec = 0 ;
         
-    }else if (sec > 59) {
+    }else if (sec > 10) {
 
         min1.innerText = min++ ;
         sec = 0 ;
     }
     
     
-}
-
-
-let interval =   setInterval(timer , 10);
-})
-
-btn3.addEventListener('click',function () {
-   
-    // min1.innerText = 0 ;
-    // sec1.innerText = 0 ;
-    // msec1.innerText = 0 ;
-//    clearInterval(interval) = 0 ;  
-    min = 0 ;
-    sec = 0 ;
-    msec = 0 ;
-
-
-
+//}
+}, 10)
+ //interval =   setInterval(timer , 10);
 })
 
 
 
 btn2.addEventListener('click',function () {
- 
-      
+    btn1.disabled = false ;
+
+    clearInterval(interval)
+    
 })
+
+btn3.addEventListener('click',function () {
+    btn1.disabled = false ;
+    msec = 0 ;
+    min = 0 ;
+    sec = 0 ;
+    msec1.innerText = 0 ;
+    min1.innerText = 0 ;
+    sec1.innerText = 0 ;
+
+    clearInterval(interval)
+   
+    
+
+})
+
+
